99d3356bce9a472c959ea0660840a781a571fd64
Alejandro Costa <alejandroesfc@gmail.com>
2023-02-12 22:30:34 -0300

    initial commit

---
+++ src/main/java/dev/acosta/tradesmart/TradesmartApplication.java
@@ -0,0 +1,13 @@
+package dev.acosta.tradesmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradesmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradesmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/acosta/tradesmart/TradesmartApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.acosta.tradesmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradesmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
