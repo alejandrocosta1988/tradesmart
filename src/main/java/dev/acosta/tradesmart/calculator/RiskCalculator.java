8793b1be98efbe718408d5855a6219f4f7edd6eb
Alejandro Costa <alejandroesfc@gmail.com>
2023-02-14 08:01:29 -0300

    sets total capital in the calculator

--- src/main/java/dev/acosta/tradesmart/calculator/RiskCalculator.java
+++ src/main/java/dev/acosta/tradesmart/calculator/RiskCalculator.java
@@ -4,7 +4,16 @@ import java.math.BigDecimal;
 
 public class RiskCalculator {
 
+    private BigDecimal totalCapital = new BigDecimal(Double.toString(0d));
     private BigDecimal riskPerTrade = new BigDecimal(Double.toString(0.01));
+
+    public BigDecimal getTotalCapital() {
+        return totalCapital;
+    }
+
+    public void setTotalCapital(double newCapital) {
+        totalCapital = new BigDecimal(Double.toString(newCapital));
+    }
 
     public BigDecimal getRiskPerTrade() {
         return riskPerTrade;
@@ -13,4 +22,6 @@ public class RiskCalculator {
     public void setRiskPerTrade(double customRiskPerTrade) {
         riskPerTrade = new BigDecimal(Double.toString(customRiskPerTrade));
     }
+
+
 }
--- src/test/java/dev/acosta/tradesmart/calculator/RiskCalculatorTest.java
+++ src/test/java/dev/acosta/tradesmart/calculator/RiskCalculatorTest.java
@@ -12,21 +12,34 @@ public class RiskCalculatorTest {
 
     private RiskCalculator calculator;
 
+    private final BigDecimal DEFAULT_RISK_PER_TRADE = new BigDecimal(Double.toString(0.01));
+    private final BigDecimal CUSTOMIZED_RISK_PER_TRADE = new BigDecimal(Double.toString(0.05));
+
     @BeforeEach
     public void setCalculator() {
         calculator = new RiskCalculator();
     }
 
     @Test
+    void givenANewCalculator_ThenTotalCapitalIsSetToZero() {
+        assertEquals(calculator.getTotalCapital(), new BigDecimal(Double.toString(0d)));
+    }
+
+    @Test
+    void givenACalculator_ThenTotalCapitalCanBeSet() {
+        BigDecimal newCapital = new BigDecimal(Double.toString(4300d));
+        calculator.setTotalCapital(4300.00);
+        assertEquals(calculator.getTotalCapital(), newCapital);
+    }
+
+    @Test
     void givenATotalCapital_ThenRiskPerTradeIsSetAtOnePercentByDefault() {
-        BigDecimal defaultRiskPerTrade = new BigDecimal(Double.toString(0.01));
-        assertEquals(calculator.getRiskPerTrade(), defaultRiskPerTrade);
+        assertEquals(calculator.getRiskPerTrade(), DEFAULT_RISK_PER_TRADE);
     }
 
     @Test
     void givenACalculator_ThenRiskPerTradeCanBeSet() {
         calculator.setRiskPerTrade(0.05);
-        BigDecimal customizedRiskPerTrade = new BigDecimal(Double.toString(0.05));
-        assertEquals(calculator.getRiskPerTrade(), customizedRiskPerTrade);
+        assertEquals(calculator.getRiskPerTrade(), CUSTOMIZED_RISK_PER_TRADE);
     }
 }
